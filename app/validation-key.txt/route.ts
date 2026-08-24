23a607d2a91104082bfb2c0621f255408c215120
Sherif <sherif@users.noreply.github.com>
2026-08-24 18:56:07 +0200

    Update Pi domain validation key

--- app/validation-key.txt/route.ts
+++ app/validation-key.txt/route.ts
@@ -1,4 +1,4 @@
-const VALIDATION_KEY = "5d7e6e7715b27227ab027711c994fe6a002210ff";
+const VALIDATION_KEY = "54d0e8bd2826d4e841d24b99234855aa6a0a1bcafbc6552a03480e6dc64717ecf6f86ff5679a83489e170c62";
 
 export async function GET() {
   return new Response(`${VALIDATION_KEY}\n`, {
